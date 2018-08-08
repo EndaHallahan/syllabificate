658947082da66bf4ad1517044a02169c88bb9b9d
rb_ <41527196+EndaHallahan@users.noreply.github.com>
2018-08-07 19:18:51 -0500

    Counting improvements, re-worked passage tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,9 @@ const assert = require('assert');
 const syl = require('../index');
 const singleTests = require('./singleTests.json');
 
+//General test case
+assert(syl.countSyllables("a") === 1,"Something has gone horribly wrong. The counter is failing catastrophically. Check for syntax errors.");
+
 //Test case insensitivity
 assert(syl.countSyllables("ReAliZEd") === 3,"Count failed on test 'ReAliZEd': case insensitivity faliure, got "+syl.countSyllables("ReAliZEd"));
 
@@ -36,31 +39,29 @@ Object.keys(singleTests).forEach(key =>
 	);
 
 //Accuracy tests, passages
-//1
-assert(syl.countSyllables(`The Threnody bled into the Requiem. The strings of the Nightbringer rang shrilly, harshly, like my gnashing teeth. 
-	I lifted my head and glared into the swirling tempests. Lightning radiated all around me, but the thunder of the unsung realm wasn't enough to drown out my shouting voice.
-	`) === 72, "Count failed on passage test 1: expected 72");
-assert(syl.countSyllablesAndPolys(`The Threnody bled into the Requiem. The strings of the Nightbringer rang shrilly, harshly, like my gnashing teeth. 
-	I lifted my head and glared into the swirling tempests. Lightning radiated all around me, but the thunder of the unsung realm wasn't enough to drown out my shouting voice.
-	`)[0] === 72, "AndPoly failed on passage test 1: expected 72");
+const pTestOne = `The Threnody bled into the Requiem. The strings of the Nightbringer rang shrilly, harshly, like my gnashing teeth. 
+	I lifted my head and glared into the swirling tempests. Lightning radiated all around me, but the thunder of the unsung realm wasn't enough to drown out my shouting voice.` // 72 syllables
 
-//2
-assert(syl.countSyllables(`We were somewhere around Barstow on the edge of the desert when the drugs began to take hold. I remember saying something like “I feel a bit lightheaded; maybe you should drive…” 
+const pTestTwo = `We were somewhere around Barstow on the edge of the desert when the drugs began to take hold. I remember saying something like “I feel a bit lightheaded; maybe you should drive…” 
 	And suddenly there was a terrible roar all around us and the sky was full of what looked like huge bats, all swooping and screeching and diving around the car, which was going about 
-	a hundred miles an hour with the top down to Las Vegas.`) === 103, "Count Failed on passage test 2: expected 103");
-assert(syl.countSyllablesAndPolys(`We were somewhere around Barstow on the edge of the desert when the drugs began to take hold. I remember saying something like “I feel a bit lightheaded; maybe you should drive…” 
-	And suddenly there was a terrible roar all around us and the sky was full of what looked like huge bats, all swooping and screeching and diving around the car, which was going about 
-	a hundred miles an hour with the top down to Las Vegas.`)[0] === 103, "AndPoly failed on passage test 2: expected 103");
+	a hundred miles an hour with the top down to Las Vegas.` //103 syllables
 
-//3
-assert(syl.countSyllables(`In the not too distant future, next sunday A.D. There was a guy named Joel. Not too different from you or me!`) === 27, "Count failed on passage test 3: expected 27")
-assert(syl.countSyllablesAndPolys(`In the not too distant future, next sunday A.D. There was a guy named Joel. Not too different from you or me!`)[0] === 27, "AndPoly failed on passage test 3: expected 27")
+const pTestThree = `In the not too distant future, next sunday A.D. There was a guy named Joel. Not too different from you or me!` //28 syllables
 
-//4
-assert(syl.countSyllables(`That was when she heard sirens, which at first she thought were municipal censorship to spare regular citizens from having to hear this kind of talk on community airtime, 
-	but then she realized were actually on the road behind her.`) === 61, "Count failed on passage 4: expected 61")
-assert(syl.countSyllablesAndPolys(`That was when she heard sirens, which at first she thought were municipal censorship to spare regular citizens from having to hear this kind of talk on community airtime, 
-	but then she realized were actually on the road behind her.`)[0] === 61, "AndPoly failed on passage 4: expected 61")
+const pTestFour = `That was when she heard sirens, which at first she thought were municipal censorship to spare regular citizens from having to hear this kind of talk on community airtime, 
+	but then she realized were actually on the road behind her.` //61 syllables
+
+assert(syl.countSyllables(pTestOne) === 72, "Count failed on passage test 1: expected 72, got " + syl.countSyllables(pTestOne));
+assert(syl.countSyllablesAndPolys(pTestOne)[0] === 72, "AndPoly failed on passage test 1: expected 72, got " + syl.countSyllablesAndPolys(pTestOne));
+
+assert(syl.countSyllables(pTestTwo) === 103, "Count Failed on passage test 2: expected 103, got " + syl.countSyllables(pTestTwo));
+assert(syl.countSyllablesAndPolys(pTestTwo)[0] === 103, "AndPoly failed on passage test 2: expected 103, got " + syl.countSyllablesAndPolys(pTestTwo));
+
+assert(syl.countSyllables(pTestThree) === 27, "Count failed on passage test 3: expected 27, got " + syl.countSyllables(pTestThree))
+assert(syl.countSyllablesAndPolys(pTestThree)[0] === 27, "AndPoly failed on passage test 3: expected 27, got " + syl.countSyllablesAndPolys(pTestThree))
+
+assert(syl.countSyllables(pTestfour) === 61, "Count failed on passage 4: expected 61, got " + syl.countSyllables(pTestFour))
+assert(syl.countSyllablesAndPolys(pTestFour)[0] === 61, "AndPoly failed on passage 4: expected 61, got " + syl.countSyllablesAndPolys(pTestFour))
 
 
 console.log("All tests passed!");
